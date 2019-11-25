f2ddb365817cc0266a2ba97212d1bb6dbde4b7d3
Ilya <54371432+ilyaLihota@users.noreply.github.com>
2019-11-25 10:37:33 +0300

    Update celery.py

--- django_redis_celery/celery.py
+++ django_redis_celery/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_redis_celery.settings')
 
 app = Celery('study')
 
