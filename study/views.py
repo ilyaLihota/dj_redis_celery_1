414f02ac88ffc9633a82d553cda3912a8d14ff3b
ilichota <ilya.lichota@gmail.com>
2019-11-28 13:45:26 +0300

    Rewrite a view as FormView.

--- study/views.py
+++ study/views.py
@@ -3,12 +3,14 @@ import redis
 
 from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.views import View
 from functools import reduce
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 
 from .forms import CalculateForm
 from .tasks import send_email_to_me
+
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -18,76 +20,74 @@ r = redis.StrictRedis(host=settings.REDIS_HOST,
                       db=settings.REDIS_DB)
 
 
-class CalculateFactorial(View):
+class CalculateFactorial(FormView):
+    """
+    Calculates a factorial of number.
+    """
+    form_class = CalculateForm
+    template_name = 'study/base.html'
+    success_url = reverse_lazy('calculate')
 
-    def get(self, request):
-
+    def get(self, request, *args, **kwargs):
         try:
-            total_views = r.incr('page:views')
+            r.incr('page:views')
         except:
-            total_views = r.set('page:views', 0)
+            r.set('page:views', 0)
             logger.error('Wrong type of page:views! Set 0.')
-
-        form = CalculateForm()
         logger.info('Get a page study/.')
+        return self.render_to_response(self.get_context_data())
 
-        return render(request, 'study/base.html', {'form': form,
-                                                   'total_views': total_views,
-                                                   })
-
-    def post(self, request):
-
-        bound_form = CalculateForm(request.POST)
-        logger.info('Get a completed form.')
-
-        if bound_form.is_valid():
-            try:
-                total_views = int(r.get('page:views'))
-            except:
-                total_views = r.set('page:views', 0)
-                logger.error('Wrong type of page:views! Set 0.')
-
-            number = bound_form.cleaned_data['value']
-
-            try:
-                number = int(number)
-            except:
-                logger.error('Failed to type integer!')
-                return HttpResponse('You should input a non-negative integer!')
-
-            if number > 0:
-                factorial = reduce(lambda x, y: x * y, range(1, number + 1))
-            elif number == 0:
-                factorial = 1
-            else:
-                logger.error('Inputed negative integer!')
-                return HttpResponse('You should input a non-negative integer!')
-
-            logger.info('Get a factorial and sent page with the result.')
-
-            return render(request, 'study/base.html', {'form': bound_form,
-                                                       'total_views': total_views,
-                                                       'number': number,
-                                                       'factorial': factorial,
-                                                       })
-        else:
-            logger.error('Form isn''t valid.')
-            form = CalculateForm()
-
-        return render(request, 'study/base.html', {'form': form})
-
-
-class SendEmail(View):
-
-    def get(self, request):
-
+    def form_valid(self, form):
         try:
             total_views = int(r.get('page:views'))
         except:
             total_views = r.set('page:views', 0)
             logger.error('Wrong type of page:views! Set 0.')
+        number = form.cleaned_data['value']
+        try:
+            number = int(number)
+        except:
+            logger.error('Failed to type integer!')
+            return HttpResponse('You should input a non-negative integer!')
+        if number > 0:
+            factorial = reduce(lambda x, y: x * y, range(1, number + 1))
+        elif number == 0:
+            factorial = 1
+        else:
+            logger.error('Inputed negative integer!')
+            return HttpResponse('You should input a non-negative integer!')
+        logger.info('Get a factorial and sent page with the result.')
+        return self.render_to_response({'form': form,
+                                        'total_views': total_views,
+                                        'number': number,
+                                        'factorial': factorial,
+                                       })
 
-        send_email_to_me(total_views)
+    def get_context_data(self, **kwargs):
+        """
+        Gets context, adds total_views to context and returns it.
+        """
+        # Get the context from parent's view.
+        context = super(CalculateFactorial, self).get_context_data(**kwargs)
+        # Add to the context our data.
+        try:
+            context['total_views'] = int(r.get('page:views'))
+        except:
+            logger.error('Failed to type integer!')
+            context['total_views'] = r.set('page:views', 0)
+        return context
+
+
+class SendEmail(View):
+    """
+    Send email to admin with amount of views of page.
+    """
+    def get(self, request):
+        try:
+            total_views = int(r.get('page:views'))
+        except:
+            total_views = r.set('page:views', 0)
+            logger.error('Wrong type of page:views! Set 0.')
+        send_email_to_me.delay(total_views)
         logger.info('Sent email with the amount of views.')
-
         return HttpResponse('Result sent to email!')
